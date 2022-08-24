038acc10187f6cc1073e4b786baed23c0aae69ef
Ubaton <raymondNG729@gmail.com>
2022-08-24 14:51:16 +0200

    v-1.0.1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import Navbar from './components/Navbar';
 import Hero from './components/Hore';
+import About from './components/About';
+import Books from './components/Books';
 
 
 function App() {
   return (
-    <div className="bg-[url('images/hero.png')] bg-cover">
-      <Navbar/>
-      <Hero/>
-    </div>
+    <>
+      <div className="bg-[url('images/hero.png')] bg-cover">
+        <Navbar/>
+        <Hero/>
+      </div>
+
+      <About/>
+      <Books/>
+    </>
   );
 }
 
