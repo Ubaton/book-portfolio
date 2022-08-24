cfc06dab0589f5a8f7472678fe43fd156c46960e
Ubaton <raymondNG729@gmail.com>
2022-08-24 13:13:54 +0200

    v-1.0.0

--- src/components/Hore.jsx
+++ src/components/Hore.jsx
@@ -6,7 +6,13 @@ const Hore = () => {
       <div className='max-w-[800px] mt-[96px] w-full h-screen mx-24 text-left flex flex-col text-5xl'>
         <p className='text-brown-700 font-vng font-bold '>A place to read</p><br></br>
         <p className='text-brown-700 font-vng font-bold '>and relax your mind</p>
-        <h1 className='text-brown-900 font-rs font-bold mt-10'>Feed your Toughts</h1>
+        <h1 className='text-brown-600 font-rs font-bold mt-10'>Feed your Toughts</h1>
+
+      <div className='flex justify-left'>
+        <button class="bg-lightbrown-500 hover:bg-brown-600 text-black py-2 px-5 rounded-full mt-12 text-sm drop-shadow-md">
+          Order
+        </button>
+      </div>
 
       </div>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,17 +9,17 @@ const Navbar = () => {
     }
     
   return (
-    <div className=''>
+    <div className='bg-brown-500 mx-10 rounded-full drop-shadow-sm'>
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4'>
-      <h1 className='text-4xl font-rs'>Boitumelo.</h1>
+      <h1 className='text-4xl font-rs text-black'>Boitumelo.</h1>
       <ul className='hidden md:flex justify-center font-vng text-[35px]'>
-        <li className='p-4'>Home</li>
-        <li className='p-4'>About</li>
-        <li className='p-4'>Books</li>
+        <li className='p-4 hover:text-brown-900'>Home</li>
+        <li className='p-4 hover:text-brown-900'>About</li>
+        <li className='p-4 hover:text-brown-900'>Books</li>
       </ul>
      
       <div className='hidden md:flex justify-end'>
-      <button class="bg-lightbrown-500 hover:bg-brown-500 text-black py-1 px-5 rounded-full">
+      <button class="bg-lightbrown-500 hover:bg-brown-600 text-black py-1 px-5 rounded-full drop-shadow-md">
         Login
       </button>
       </div>
@@ -28,18 +28,18 @@ const Navbar = () => {
          {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/> }
       </div>
 
-      <div className={!nav ? 'fixed left-0 top-0 mt-24 w-[60%] border-r font-vng text-[25px] border-r-brown-500 h-full bg-brown-500 ease-in-out duration-500' : 'fixed left-[-100%] top-[10%]'}>
+      <div className={!nav ? 'fixed left-0 top-0 mt-24 w-[60%] border-r text-[25px] border-r-brown-500 h-full bg-brown-500 ease-in-out duration-500' : 'fixed left-[-100%] top-[10%]'}>
       {/* <h1 className='text-4xl font-rs py-3 m-4'>Boitumelo.</h1> */}
-        <ul className='pt-18 p-4 '>
+        <ul className='pt-18 p-4 font-vng'>
         <li className='p-4 border-b border-lightbrown-500'>Home</li>
         <li className='p-4 border-b border-lightbrown-500'>About</li>
         <li className='p-4 border-b border-lightbrown-500'>Books</li> 
         </ul>
 
-        <div className='flex justify-center'>
-        <button className='bg-lightbrown-500 h-10 w-10'>
+        <div className='hidden md:flex justify-center'>
+          <button class="bg-lightbrown-500 hover:bg-brown-500 text-black py-1 px-5 rounded-full drop-shadow-lg">
             Login
-        </button>
+          </button>
         </div>
 
       </div>
