e4b7cca1dde9f76c80cfdd2783ebcf295316961f
Ubaton <raymondNG729@gmail.com>
2022-09-02 20:16:51 +0200

    v-1.0.14

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -10,8 +10,12 @@ const Newsletter = () => {
         </div>
         <div className='my-4'>
           <div className='flex flex-col sm:flex-row items-center justify-between w-full'>
-            <input className='rounded-full px-4 py-3' type="email" placeholder='Enter Email' />
-            <button className=' hover:bg-brown-600 drop-shadow-md bg-lightbrown-600 text-black rounded-full font-semibold w-[200px ml-4 my-6 px-6 py-3'>Notify Me</button>
+            
+              
+              <input type="email" id="helper-text" aria-describedby="helper-text-explanation" class="bg-brown-50 border border-brown-300 text-brown-900 text-sm rounded-full focus:ring-brown-500 focus:border-brown-500 block w-full p-2.5  dark:bg-lightbrown-400 dark:border-lightbrown-200 dark:placeholder-brown-800 dark:text-brown-800 dark:focus:ring-brown-700 dark:focus:border-brown-700" placeholder="Enter Email"/>
+              
+
+            <button className=' hover:bg-brown-600 drop-shadow-md bg-lightbrown-600 text-white rounded-full font-semibold w-[200px] ml-4 my-6 px-6 py-3'>Notify Me</button>
           </div>
           <p> We care about the protection of your data. Read our <span className=' text-brown-900 cursor-pointer'>Privacy Policy.</span></p>
         </div>
