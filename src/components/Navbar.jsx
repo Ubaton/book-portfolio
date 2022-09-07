9ef02c97ae7bdccb7a3c05ffbfebbe2acab98c6a
Ubaton <raymondNG729@gmail.com>
2022-09-07 10:38:10 +0200

    v-1.0.17

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,9 +14,9 @@ const Navbar = () => {
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4'>
       <h1 className='text-4xl font-rs cursor-pointer '>Boitumelo.</h1>
       <ul className='hidden md:flex justify-center font-vng text-[35px]'>
-        <li className='p-4 hover:text-brown-900 cursor-pointer '>Home</li>
-        <li className='p-4 hover:text-brown-900 cursor-pointer '>About</li>
-        <li className='p-4 hover:text-brown-900 cursor-pointer '>Books</li>
+        <li className='p-4 cursor-pointer hover:text-brown-900'><a href="Home">Home</a></li>
+        <li className='p-4 cursor-pointer hover:text-brown-900'><a href="About">About</a></li>
+        <li className='p-4 cursor-pointer hover:text-brown-900'><a href="Books">Books</a></li>
       </ul>
      
       <div className='hidden md:flex justify-end z-10 w-44 rounded'>
@@ -32,13 +32,13 @@ const Navbar = () => {
       <div className={nav ? 'bg-brown-500 h-120 py-4 rounded-md fixed left-0 top-[2px] mt-24 w-[50%] text-[25px] ease-in-out duration-500' : 'fixed left-[-100%] top-[-10%]'}>
       
         <ul className='pt-18 p-4 font-vng'>
-        <li className='p-4 border-b border-lightbrown-500 cursor-pointer hover:text-brown-900'>Home</li>
-        <li className='p-4 border-b border-lightbrown-500 cursor-pointer hover:text-brown-900'>About</li>
-        <li className='p-4 border-b border-lightbrown-500 cursor-pointer hover:text-brown-900'>Books</li> 
+          <li className='p-4 border-b border-lightbrown-500 cursor-pointer hover:text-brown-900'><a href="Home">Home</a></li>
+          <li className='p-4 border-b border-lightbrown-500 cursor-pointer hover:text-brown-900'><a href="About">About</a></li>
+          <li className='p-4 border-b border-lightbrown-500 cursor-pointer hover:text-brown-900'><a href="Books">Books</a></li> 
         </ul>
 
-        <div className='flex justify-center'>
-          <button className="bg-lightbrown-500 text-[18px] hover:bg-brown-500 text-black py-1 px-[15px] rounded-full drop-shadow-lg transition-all">
+        <div className='flex justify-center cursor-pointer'>
+          <button className="bg-lightbrown-500 text-[18px] cursor-pointer hover:bg-brown-500 text-black py-1 px-[15px] rounded-full drop-shadow-lg transition-all">
             Login
           </button>
         </div>
