db9212dcbe90ab352f071df067efcb91982f0b97
Ubaton <raymondNG729@gmail.com>
2023-11-15 17:18:44 +0200

    v0.1.5

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -4,15 +4,15 @@ const Blog = () => {
   return (
     <div className="container mx-auto bg-lightblue-500">
       <div className="max-w-[1240px] mx-auto py-8 px-4">
-        <h1 className="text-4xl font-bold mb-6">Blog</h1>
+        <h1 className="text-4xl text-brown-500 font-bold mb-6">Blog</h1>
 
         {/* Add your blog posts here */}
-        <div className="flex flex-wrap -mx-4">
+        <div className="flex flex-wrap mx-4 text-brown-900">
           {/* Example Blog Post */}
           <div className="w-full md:w-1/2 lg:w-1/3 px-4 mb-8 hover:scale-105 transition-transform duration-300 ease-in-out">
             <div className="bg-brown-500 rounded-lg p-6 shadow-lg">
               <h2 className="text-2xl font-bold mb-4">Blog Post Title</h2>
-              <p className="text-gray-700">
+              <p className="">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Curabitur tristique justo vel accumsan cursus.
               </p>
@@ -21,7 +21,7 @@ const Blog = () => {
           <div className="w-full md:w-1/2 lg:w-1/3 px-4 mb-8 hover:scale-105 transition-transform duration-300 ease-in-out">
             <div className="bg-brown-500 rounded-lg p-6 shadow-lg">
               <h2 className="text-2xl font-bold mb-4">Blog Post Title</h2>
-              <p className="text-gray-700">
+              <p className="">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Curabitur tristique justo vel accumsan cursus.
               </p>
@@ -30,7 +30,7 @@ const Blog = () => {
           <div className="w-full md:w-1/2 lg:w-1/3 px-4 mb-8 hover:scale-105 transition-transform duration-300 ease-in-out">
             <div className="bg-brown-500 rounded-lg p-6 shadow-lg">
               <h2 className="text-2xl font-bold mb-4">Blog Post Title</h2>
-              <p className="text-gray-700">
+              <p className="">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Curabitur tristique justo vel accumsan cursus.
               </p>
@@ -39,7 +39,7 @@ const Blog = () => {
           <div className="w-full md:w-1/2 lg:w-1/3 px-4 mb-8 hover:scale-105 transition-transform duration-300 ease-in-out">
             <div className="bg-brown-500 rounded-lg p-6 shadow-lg">
               <h2 className="text-2xl font-bold mb-4">Blog Post Title</h2>
-              <p className="text-gray-700">
+              <p className="">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Curabitur tristique justo vel accumsan cursus.
               </p>
@@ -48,7 +48,7 @@ const Blog = () => {
           <div className="w-full md:w-1/2 lg:w-1/3 px-4 mb-8 hover:scale-105 transition-transform duration-300 ease-in-out">
             <div className="bg-brown-500 rounded-lg p-6 shadow-lg">
               <h2 className="text-2xl font-bold mb-4">Blog Post Title</h2>
-              <p className="text-gray-700">
+              <p className="">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Curabitur tristique justo vel accumsan cursus.
               </p>
@@ -57,7 +57,7 @@ const Blog = () => {
           <div className="w-full md:w-1/2 lg:w-1/3 px-4 mb-8 hover:scale-105 transition-transform duration-300 ease-in-out">
             <div className="bg-brown-500 rounded-lg p-6 shadow-lg">
               <h2 className="text-2xl font-bold mb-4">Blog Post Title</h2>
-              <p className="text-gray-700">
+              <p className="">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Curabitur tristique justo vel accumsan cursus.
               </p>
