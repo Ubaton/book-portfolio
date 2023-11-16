b335f42188679d16bbbef9cd6bc29b0699a281ea
Ubaton <raymondNG729@gmail.com>
2023-11-16 15:56:31 +0200

    v0.1.9

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,6 +1,29 @@
 import React from "react";
+import emailjs from "emailjs-com";
 
 const Newsletter = () => {
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    // Get the user's email from the input field
+    const userEmail = document.getElementById("emailInput").value;
+
+    // Your EmailJS template ID, service ID, and user ID
+    const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+    const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+    const userId = process.env.REACT_APP_EMAILJS_USER_ID;
+
+    // Send the email using EmailJS
+    emailjs
+      .send(serviceId, templateId, { to_email: userEmail }, userId)
+      .then((response) => {
+        console.log("Email sent successfully:", response);
+      })
+      .catch((error) => {
+        console.error("Error sending email:", error);
+      });
+  };
+
   return (
     <div className="container mx-auto py-8 text-white bg-brown-500 rounded-lg px-4">
       <div className="grid grid-cols-1 lg:grid-cols-3 max-w-[1240px] mx-auto">
@@ -11,17 +34,22 @@ const Newsletter = () => {
           <p>Sign up for my newsletter and stay up to date.</p>
         </div>
         <div className="my-4">
-          <div className="flex flex-col sm:flex-row items-center justify-between w-full">
-            <input
-              type="email"
-              id="emailInput"
-              className="bg-lightwhite-500 border border-brown-300 text-brown-900 text-sm rounded-full focus:ring-brown-500 focus:border-brown-500 block w-full p-2.5 dark:bg-lightbrown-400 dark:border-lightbrown-200 dark:placeholder-brown-800 dark:text-brown-800 dark:focus:ring-brown-700 dark:focus:border-brown-700"
-              placeholder="Enter Email"
-            />
-            <button className="hover:bg-brown-600 drop-shadow-md bg-lightbrown-600 text-white rounded-full font-semibold w-full md:w-[200px] ml-0 sm:ml-4 my-6 px-6 py-3 transition-all">
-              Notify Me
-            </button>
-          </div>
+          <form onSubmit={sendEmail}>
+            <div className="flex flex-col sm:flex-row items-center justify-between w-full">
+              <input
+                type="email"
+                id="emailInput"
+                className="bg-lightwhite-500 border border-brown-300 text-brown-900 text-sm rounded-full focus:ring-brown-500 focus:border-brown-500 block w-full p-2.5 dark:bg-lightbrown-400 dark:border-lightbrown-200 dark:placeholder-brown-800 dark:text-brown-800 dark:focus:ring-brown-700 dark:focus:border-brown-700"
+                placeholder="Enter Email"
+              />
+              <button
+                type="submit"
+                className="hover:bg-brown-600 drop-shadow-md bg-lightbrown-600 text-white rounded-full font-semibold w-full md:w-[200px] ml-0 sm:ml-4 my-6 px-6 py-3 transition-all"
+              >
+                Notify Me
+              </button>
+            </div>
+          </form>
           <p>
             We care about the protection of your data. Read our{" "}
             <span className="text-brown-900 cursor-pointer">
