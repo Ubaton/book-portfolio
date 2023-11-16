0667805ceb23f7efdb789b217654d4d4e03e5f54
Ubaton <raymondNG729@gmail.com>
2023-11-16 15:34:08 +0200

    v0.1.9

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -10,25 +10,22 @@ const Books = () => {
             Books
           </h1>
           <p className="py-5 px-4 md:px-0 text-lg md:text-2xl text-white">
-            Growing up in an environment full of expectations, as a young
-            Christian girl Boitumelo lives up to those expectations. She put her
-            faith in Christ in her teen years and thought that with the right
-            foundation (Jesus) she would never be a disappointment in any way to
-            those thinking highly of her including herself. But life doesn't
-            turn out as expected when she makes bad choices and ends up leading
-            a life of anxiety, depression, and anger. Today she has overcome and
-            is learning to daily choose to forgive herself for her past mistakes
-            because they do not define her. Anger issues? Are you living in
-            anger and resentment towards yourself, others, or God? Do you
-            struggle to overcome worry, depression, and unforgiveness all
-            resulting in anger issues? This book is for your breakthrough. Stop
-            And Start is a learning tool about:
-            <ul>
-              <li>*Our cognitive behavior to Anger</li>
-              <li>*Reasons why we are entrapped by Anger</li>
-              <li>* Our solution way tobreaking free from Ange</li>
-            </ul>
+            Boitumelo, a young Christian girl, faced the pressure of living up
+            to high expectations in her environment. Despite putting her faith
+            in Christ during her teen years, she experienced unexpected
+            challenges, making bad choices that led to a life marked by anxiety,
+            depression, and anger. Over time, she managed to overcome these
+            struggles and learned to daily forgive herself, recognizing that her
+            past mistakes don't define her. "Stop And Start" as a learning tool
+            specifically designed for those dealing with anger, resentment,
+            worry, depression, and unforgiveness, offering a pathway to
+            breakthroughs in these areas:
           </p>
+          <ul className="py-5 px-4 md:px-0 text-lg md:text-2xl text-white">
+            <li>*Our cognitive behavior to Anger</li>
+            <li>*Reasons why we are entrapped by Anger</li>
+            <li>* Our solution way tobreaking free from Anger</li>
+          </ul>
         </div>
         <div className="hover:scale-105 transition-transform duration-300 ease-in-out">
           <img
